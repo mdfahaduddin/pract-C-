6206634e6943a0b107547025aa4e5c174f80cced
Md Fahad Uddin <mdfahaduddin182803@gmail.com>
2022-10-28 10:57:51 +0600

    constructor create

--- classOPP.cpp
+++ classOPP.cpp
@@ -9,11 +9,19 @@ public :
     double cgpa;
 
     //param function create for input data
-    void input(int i,string p, double c){
+   // void input(int i,string p, double c){
+  //  id = i;
+   // phnNo = p;
+  //  cgpa = c;
+  //  }
+
+  //created a constructor exact same name of class
+  students(int i,string p, double c){
     id = i;
     phnNo = p;
     cgpa = c;
-    }
+  }
+
 
     //function create for display
     void display(){
@@ -25,12 +33,12 @@ public :
 };
 
 int main(){
-    students fahad,abir; //create object fahad
+    students fahad(2110317006,"01609671003",3.99),abir(2110318006,"016000000",4.00); //create object fahad
 
-    fahad.input(2110317006,"01609671003",3.99);//call input function
+   // fahad.input(2110317006,"01609671003",3.99);//call input function
     fahad.display();
 
-    abir.input(2110318006,"016000000",4.00);//call display function
+  //  abir.input(2110318006,"016000000",4.00);//call display function
     abir.display();
 
 
