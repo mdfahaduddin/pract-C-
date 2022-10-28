0a971cd58ffb4b20f58b9b1065abd5b280d55399
Md Fahad Uddin <mdfahaduddin182803@gmail.com>
2022-10-28 10:19:42 +0600

    2nd

--- classOPP.cpp
+++ classOPP.cpp
@@ -11,12 +11,20 @@ public :
 };
 
 int main(){
-    students fahad;
+    students fahad,abir; //create object fahad
     fahad.id = 2110317006;
     fahad.phnNo = "01609671003";
     fahad.cgpa = 3.99;
 
+    abir.id = 2110318006;
+    abir.phnNo = "0160000000";
+    abir.cgpa = 4;
+
    cout<<"ID : "<<fahad.id<<endl;
    cout<<"Phone Number : "<<fahad.phnNo<<endl;
    cout<<"CGPA : "<<fahad.cgpa<<endl;
+
+   cout<<"ID : "<<abir.id<<endl;
+   cout<<"Phone Number : "<<abir.phnNo<<endl;
+   cout<<"CGPA : "<<abir.cgpa<<endl;
 }
